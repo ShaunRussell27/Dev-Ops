006fe3c73c69257fc210b7da572dd102489c5256
ShaunRussell27 <shaunrussell1111@gmail.com>
2025-10-21 22:21:29 +0100

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Demon Slayer API!");
+    }
+
+    public static String getMessage() {
+        return "Welcome to the Demon Slayer API!";
+    }
+}
