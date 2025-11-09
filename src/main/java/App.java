de3fc81436ac02128a689eadaa5364244bd30da1
ShaunRussell27 <shaunrussell1111@gmail.com>
2025-11-09 13:11:03 +0000

    Auto documentation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,10 +4,22 @@
  * passing parameters into a method.
  */
 public class App {
+
+    /**
+     * The entry point of the Demon Slayer API demo.
+     * Prints a welcome message to the console.
+     *
+     * @param args Command line arguments (not used).
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to the Demon Slayer API!");
     }
 
+    /**
+     * Returns a welcome message.
+     *
+     * @return A String containing the welcome message.
+     */
     public static String getMessage() {
         return "Welcome to the Demon Slayer API!";
     }
