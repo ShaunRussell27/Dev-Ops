52c938a74a3a763928e44905ac78985fb7224f46
ShaunRussell27 <shaunrussell1111@gmail.com>
2025-11-19 12:30:30 +0000

    Update author name in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
  * This program demonstrates logging and basic monitoring
  * using SLF4J and Micrometer.
  * 
- * @author Your Name
+ * @author Shaun Russell
  * @version 1.0
  */
 public class App {
